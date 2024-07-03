348090f0f16e6a367cf670e7f1e33f4d01d38903
ParkJungHwan <pjh0740@naver.com>
2024-07-03 23:20:31 +0900

    시크릿키 에러 수정

--- django-server/djangoserver/settings.py
+++ django-server/djangoserver/settings.py
@@ -14,8 +14,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env('SECRET_KEY')
-SECRET_KEY = "django-insecure-+$l!=45o4l#%%@lw&0ma%d&j%a43i%j4ns_c=f@6^+29n0(0ig"
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
