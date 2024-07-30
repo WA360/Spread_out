46fc6ddfcec9940671c12cdcbc107d1bdf8f6f2f
ParkJungHwan <pjh0740@naver.com>
2024-07-30 15:47:03 +0900

    서브모듈로 업데이트

--- django-server/README.md
+++
@@ -1,9 +0,0 @@
-킹갓제
-하위테스트
-
-**가상환경**
-python -m venv venv
-.\venv\Scripts\activate
-
-**라이브러리 설치**
-pip install -r requirements.txt
--- django-server/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from api.views import RecommendView
-
-app_name = "accountapp"
-
-urlpatterns = [
-    path("recommend/", RecommendView.as_view(), name="recommend"),
-]
